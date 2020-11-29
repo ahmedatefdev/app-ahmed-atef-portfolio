0cab7f76e9586a062adb4a5530036e65ea0696a8
Ahmed Atef <43449355+ahmedatefdev@users.noreply.github.com>
2020-11-29 16:40:06 +0200

    added

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -4,9 +4,8 @@ import HomePage from '../index';
 import Layout from '../../components/Layout';
 
 describe('index page', () => {
-    it('should have App component', () => {
-        const subject = mount(<HomePage />);
-        expect(subject.find("Layout")).toHaveLength(1);
-    });
-    
+  it('should have App component', () => {
+    const subject = mount(<HomePage />);
+    expect(subject.find('Layout')).toHaveLength(1);
+  });
 });
